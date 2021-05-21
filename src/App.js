b7896e2e1a2c6815f7b597193be190009827b28e
Havockeyan <karthihavoc121@gmail.com>
2021-05-21 10:27:34 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import './App.css';
+import React from 'react';
+
+class App extends React.Component{
+
+   constructor(){
+     super();
+     this.state={
+       number: 0
+     }
+   }
+
+  render(){
+    return <div style={{'width': '100%',
+      'display': 'flex',
+      'flex-direction': 'row',
+      'justify-content': 'center',
+    }}>
+    
+    <div className='numbers' style={{padding: 100}}>
+        <p style={{fontSize: 150,width: '80px'}}>{this.state.number}</p>
     </div>
-  );
+  <div className='left'>   
+    <div className='minvalue' style={{padding: 50}}>
+         <label style={{color: '#AACCFF',fontSize: 20}}>Min number</label><br></br>
+         <input type='text' id='min'></input>
+    </div>
+
+    <div className='maxvalue' style={{padding: 50}}>
+    <label style={{color: '#AACCFF',fontSize: 20}}>Max number</label><br></br>
+         <input type='text' id='max'></input>
+    </div>
+    <div>  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
+      <button onClick={this.generate}>
+      Generate
+    </button>
+    </div>
+    </div>
+    </div>
+  }
+
+  generate=() =>{
+     var max = parseInt(document.getElementById('max').value);
+     var min = parseInt(document.getElementById('min').value);
+     if(max>min && min!=NaN){
+     var changenum = Math.floor(Math.random() * (max - min) + min)
+     console.log(changenum)
+     }else{
+       alert('check weather the min number is not null and less than max number')
+     }
+    this.setState({
+      number: changenum
+    })
+  }
+
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,14 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const heading = <div><h1 style={{textAlign: 'center',color: '#AACCFF'}}>Reandom number Generator </h1></div>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+
+ReactDOM.render(heading,document.getElementById('head'))
+
+ReactDOM.render(<App />,document.getElementById('root'))
+
 reportWebVitals();
